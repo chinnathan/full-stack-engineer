08761d377da6def52037220e08f7a62881544a03
Chinnathan Chongsujiphan <chinnathan@gmail.com>
2022-06-05 21:50:43 +0700

    chore: initiate mono repository

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: "module",
+    ecmaFeatures: {
+      "jsx": true
+    }
+  },
+};
