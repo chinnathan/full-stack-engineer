2dab9e92b8ba188006a84c262338ef88b975d378
Chinnathan Chongsujiphan <chinnathan@gmail.com>
2022-06-07 02:33:47 +0700

    test: change and run sample jest

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return expected message', () => {
+      expect(appController.getHello()).toBe('Bootstrap and playground of becoming full stack developer!');
     });
   });
 });
