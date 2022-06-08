f333e2b4666ac52f46d269a79f0e83699d0ac5bf
Chinnathan Chongsujiphan <chinnathan@gmail.com>
2022-06-09 02:25:00 +0700

    ops: temporally enable cors before implementing nginx reverse proxy

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Setting up a reverse proxy server with nginx later
+  app.enableCors(); // temporally enable cors
   await app.listen(3001);
 }
 bootstrap();
